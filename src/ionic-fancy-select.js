a258e37ea807e554ca3a30212ef623671206f772
John Bayly <pl@tipstrade.net>
2015-04-16 18:56:42 +0100

    Use watch to check for value changes and update the text
    Also add callback for when the value has changed, as well as a callback for
    providing custom text

--- src/ionic-fancy-select.js
+++ src/ionic-fancy-select.js
@@ -24,8 +24,10 @@ angular.module("ionic-fancy-select", ["ionic"])
 
     // The default attribute set
     scope: {
-      items: "=",
-      value: "="
+      items: "=", // Needs to have a value
+      value: "=", // Needs to have a value
+      valueChangedCallback: "&valueChanged", // The callback used to signal that the value has changed
+      getCustomTextCallback: "&getCustomText" // The callback used to get custom text based on the selected value
     },
 
     // Hook up the directive
@@ -86,15 +88,76 @@ angular.module("ionic-fancy-select", ["ionic"])
       scope.$on("$destroy", function() {
         scope.modal.remove();
       });
+      
+      // Gets the text for the specified values
+      scope.getText = function(value) {
+        // Push the values into a temporary array so that they can be iterated through
+        var temp;
+        if (scope.multiSelect) {
+          temp = value ? value : []; // In case it hasn't been defined yet
+        } else {
+          temp = (value === null || (typeof value === "undefined")) ? [] : [value]; // Make sure it's in an array, anything other than null/undefined is ok
+        }
+
+        var text = "";
+        if (temp.length) {
+          // Concatenate the list of selected items
+          angular.forEach(scope.items, function(item, key) {
+            for (var i = 0; i < temp.length; i++) {
+              if (item[scope.valueProperty] == temp[i]) {
+                text += (text.length ? ", " : "") + item[scope.textProperty];
+                break;
+              }
+            }
+          });
+          
+        } else {
+          // Just use the default text
+          text = scope.defaultText;
+          
+        }
+
+        // If a callback has been specified for the text
+        return scope.getCustomTextCallback({value: value}) || text;
+      };
 
       // Hides the list
       scope.hideItems = function(event) {
         scope.modal.hide();
       };
+      
+      // Raised by watch when the value changes
+      scope.onValueChanged = function(newValue, oldValue) {
+        scope.text = scope.getText(newValue);
+        
+        // Notify subscribers that the value has changed
+        scope.valueChangedCallback({value: newValue});
+      };
 
       // Shows the list
       scope.showItems = function(event) {
         event.preventDefault(); // Prevent the event from bubbling
+        
+        // For multi-select, make sure we have an up-to-date list of checked items
+        if (scope.multiSelect) {
+          // Clone the list of values, as we'll splice them as we go through to reduce loops
+          var values = scope.value ? angular.copy(scope.value) : [];
+          
+          angular.forEach(scope.items, function(item, key) {
+            // Not checked by default
+            item[scope.checkedProperty] = false;
+            
+            var val = item[scope.valueProperty];
+            for (var i = 0; i < values.length; i++) {
+              if (val === values[i]) {
+                item[scope.checkedProperty] = true;
+                values.splice(i, 0); // Remove it from the temporary list
+                break;
+              }
+            }
+          });
+        }
+
         scope.modal.show();
       };
 
@@ -102,47 +165,27 @@ angular.module("ionic-fancy-select", ["ionic"])
       scope.validate = function(item) {
         if (scope.multiSelect) {
           // Need to scan the list for selected items and push them into the value list
-          scope.text = "";
           scope.value = [];
 
           if (scope.items) {
             angular.forEach(scope.items, function(item, key) {
               if (item[scope.checkedProperty]) {
-                scope.text += (scope.text.length === 0 ? "" : ", ") + item[scope.textProperty];
-                scope.value[scope.value.length] = item;
+                scope.value[scope.value.length] = item[scope.valueProperty];
               }
             });
           }
 
         } else {
           // Just use the current item
-          scope.text = item[scope.textProperty];
           scope.value = item[scope.valueProperty];
 
         }
 
-        // If no item has been selected yet
-        if (!scope.value) {
-          // If a value is required, items are required too
-          if (!scope.allowEmpty && scope.items && scope.items.length) {
-            var first = scope.items[0];
-
-            scope.value = first;
-            scope.text = first[scope.textProperty];
-
-            // So the checked field is set
-            if (scope.multiSelect) {
-              first[scope.checkedProperty] = true;
-            }
-
-          } else {
-            scope.text = scope.defaultText;
-
-          }
-        }
-
         scope.hideItems();
       };
+      
+      // Watch the value property, as this is used to build the text
+      scope.$watch(function(){return scope.value;}, scope.onValueChanged, true);
     }
   };
 })
